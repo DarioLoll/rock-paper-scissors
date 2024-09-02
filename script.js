65c5502538d772b6f2ce9f370d5781f21910247b
DarioLoll <stromajer.dario@gmail.com>
2024-09-02 21:45:07 +0200

    Make a game last 5 rounds, test computer selection choosing all 3 options equally

--- script.js
+++ script.js
@@ -2,46 +2,90 @@ let humanScore = 0;
 let computerScore = 0;
 
 console.log("Welcome to the Rock-Paper-Scissors Game!");
+testComputerChoice(3000);
+playGame();
 
-const humanSelection = getValidHumanChoice().toLowerCase();
-const computerSelection = getComputerChoice();
+function testComputerChoice(iterations) {
+    let rocks = 0;
+    let scissors = 0;
+    let papers = 0;
+    for(let i = 0; i < iterations; i++) {
+        let choice = getComputerChoice();
+        if (choice === "rock") {
+            rocks++;
+        } else if (choice === "scissors") {
+            scissors++;
+        } else if (choice === "paper") {
+            papers++;
+        } else {
+            console.log(`Error! The computer chose ${choice}`);
+            return;
+        }
+    }
+    console.log(`Test with ${iterations} iterations over.`);
+    console.log(`Result: ${rocks} rocks, ${scissors} scissors, ${papers} papers.`);
+}
 
-console.log(`You chose ${humanSelection}!`);
-console.log(`The computer chose ${computerSelection}`);
+function playGame() {
+    for(let i = 0; i < 5; i++) {
+        console.log(`Round ${i + 1}`);
+        const humanSelection = getValidHumanChoice().toLowerCase();
+        const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
-console.log(`Score: ${humanScore} -:- ${computerScore}`);
+        console.log(`You chose ${humanSelection}!`);
+        console.log(`The computer chose ${computerSelection}`);
+
+        let result = playRound(humanSelection, computerSelection);
+        processRoundResult(result);
+    }
+
+    if (humanScore > computerScore) {
+        console.log("Nice, you won!");
+    } else if (computerScore > humanScore) {
+        console.log("You lost bruh");
+    } else {
+        console.log("Draw, rematch now!");
+    }
+
+    console.log("Refresh the page to start a new game!");
+}
+
+function processRoundResult(result) {
+    if (result === 1) {
+        humanScore++;
+        console.log("You won!");
+    } else if (result === -1) {
+        computerScore++;
+        console.log("The computer won!");
+    } else {
+        console.log("It's a draw!");
+    }
+    console.log(`Score: ${humanScore} -:- ${computerScore}`);
+}
 
 function playRound(humanChoice, computerChoice) {
 
     if (humanChoice === computerChoice) {
-        console.log("It's a draw!");
-        return;
+        return 0;
     }
 
     if (humanChoice === "rock") {
         if (computerChoice === "paper") {
-            computerScore++;
-            console.log("The computer won!");
+            return -1;
         } else {
-            humanScore++;
-            console.log("You won!");
+            return 1;
         }
     } else if (humanChoice === "scissors") {
         if (computerChoice === "rock") {
-            computerScore++;
-            console.log("The computer won!");
+            return -1;
         } else {
-            humanScore++;
-            console.log("You won!");
+            return 1;
         }
     } else {
         if (computerChoice === "scissors") {
-            computerScore++;
-            console.log("The computer won!");
+            return -1;
         } else {
-            humanScore++;
-            console.log("You won!");
+            return 1;
         }
     }
 }
